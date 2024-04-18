c88b4b51527b439cae8211ad43a3e08ca16ababa
Gabor Vizi <Gabor_Vizi@mentor.com>
2024-04-18 22:15:07 +0200

    Mati Clock print

---
+++ MatiOraPython.py
@@ -0,0 +1,25 @@
+# Mati python ora script
+import time
+import math
+
+line = 0
+
+print('Első szám')
+result = input()
+
+line = int(result)
+
+def convert_line_to_clock(number):
+    hour = math.floor(number / 1000)
+    minute = math.floor(number / 10) % 100
+    second = number % 10
+    str = f'{hour}:{minute:02d}:{second:02d}'
+    need_skip = True if minute >= 60 else False
+    return str, need_skip
+
+while True:
+    clock, need_skip = convert_line_to_clock(line)
+    print(f'{line}    {clock}')
+    line += 1
+    if not need_skip:
+        time.sleep(1)
