0625f3bab0ce2f6299a6282ab0b9de9c241250cb
Gabor Vizi <gabor.vizi@siemens.com>
2025-12-14 10:51:15 +0100

    Added random number generator script

---
+++ MatiRandom.py
@@ -0,0 +1,9 @@
+import random
+import time
+
+print('Random generation kezdődik! Kérjük kapaszkodjanak!')
+
+while True:
+    generalt_szam = random.randint(1,10)
+    print(generalt_szam)
+    time.sleep(10)
