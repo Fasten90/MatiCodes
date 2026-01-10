9d790423b144ce3645076b37a188b09ac596302c
Fasten90 <fasten90@gmail.com>
2026-01-10 02:00:00 +0100

    code written by Mati

---
+++ Mati Cubess Hama.py
@@ -0,0 +1,16 @@
+Mati Cubes Hama.Py
+
+1Hama a 2, se 549B sers x2 pymit blocks, chain cube, 2248, 2048,...
+1Es étel app eszik 2-est, valamint 549B eszi x2 Blocks, chain cube, 2248, 2048 appok...
+
+Ha Hama se, novs se x2 sa.
+Ha eszik, növelődjön a x2 szám.
+
+x2 sa lita: 2 kk, 4 nsrg 16K bsa la
+x2 számlista: 2 kék, 4 narancssárga. 16K Lila.
+
+Se besr sa: 549B
+A bezáró szám: 549B
+
+Lesesa: 1B, 68B, 1a, 1M. 8-8192, 32K-524K, 2M-536M, Sapl.
+Lennének: 1B. 68B, 1a, 1M. 8-8192, 32K, 524K, 2M-536M, Nagyobbak.
